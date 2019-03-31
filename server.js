a2dd1ec3a2083ebe51d7ba32b784e9b8f9e7b799
Geoff <geoffmcohen@gmail.com>
2019-03-31 14:56:32 -0600

    Fixing port issue for heroku

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var ejs = require('ejs');
+var port = process.env.PORT || 3000;
 
 var app = express();
 app.set('views', './views');
 app.engine('html', ejs.renderFile);
-app.listen(80);
+app.listen(port);
 
 app.get('*', function(req, res){
   app.render('coming_soon.html', function(err, renderedData){
