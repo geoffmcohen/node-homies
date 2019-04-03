59955fc6c1d131775c196bdbedeaa2a65ac4331a
Geoff <geoffmcohen@gmail.com>
2019-04-03 13:42:42 -0600

    Adding database connectivity test

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ app.set('views', './views');
 app.engine('html', ejs.renderFile);
 app.listen(port);
 
+// Connect to mongodb
+var MongoClient = require('mongodb').MongoClient;
+var mongoURI = process.env.MONGOLAB_URI;
+MongoClient.connect(mongoURI, {useNewUrlParser: true}, function(err, db){
+  if(err){
+    console.log("Unable to connect to MongoDB!!!");
+    console.log(err);
+  } else {
+    console.log("Succesfully connected to MongoDB.");
+  }
+});
+
+// Route all requests to coming soon page
 app.get('*', function(req, res){
   app.render('coming_soon.html', function(err, renderedData){
       res.send(renderedData);
